7b5f34aa2099bfd202ef85fbbf114e3255fd426c
NishantChauhan_11 <26nishantchauhan@gmail.com>
2025-06-25 07:44:16 +0530

    assignment

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,1 +1,18 @@
-alert('Hello')
+const grandparent = document.getElementById("grandparent");
+const parent = document.getElementById("parent");
+const child = document.getElementById("child");
+
+grandparent.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("grandparent");
+});
+
+parent.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("parent");
+});
+
+child.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("child");
+});
